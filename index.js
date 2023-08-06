b2a8ac740437bc95427c89f5086ccce061c644b2
Roland Hesse <rolandhesse11783@gmail.com>
2023-08-06 13:30:17 +0200

    update dark-mode functionality to include header and footer (does not work)

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const bodyElement = document.querySelector('[data-js="body-element"]');
+const header = document.querySelector('[data-js="header"]');
+const footer = document.querySelector('[data-js="footer"]');
+const footerIcon = document.querySelector('[data-js="footer-icon"]');
 const darkModeButton = document.querySelector('[data-js="dark-mode-button"]');
 
 darkModeButton.addEventListener("click", () => {
   console.log("Yeah!");
   bodyElement.classList.toggle("dark-mode");
+  header.classList.toggle("dark-mode-header");
+  footer.classList.toggle("dark-mode-footer");
+  if (footerIcon.classList.contains("dark-mode-footerIcon")) {
+    footerIcon.classList.remove("dark-mode-footerIcon");
+  } else {
+    footerIcon.classList.add("dark-mode-footerIcon");
+  }
 });
